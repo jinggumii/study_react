ebc15d5102c2410dbf6f383308e1b7a137aaae00
jinggumii <jinggumi9@gmail.com>
2020-09-23 01:47:03 +0900

    day 02 food ex

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React from 'react';
 
-function Food({fav}){
-return <h1>i like {fav}</h1>;
+function Food({name, picture}){
+return (
+  <div>
+    <h2>i like {name}</h2>
+    <img src={picture} alt={name}/>
+  </div>
+  );
 }
+
+
+const foodILike = [
+  { 
+    id:1,
+    name:"Kimchi",
+    image:"https://health.chosun.com/site/data/img_dir/2020/07/21/2020072103114_0.jpg"
+  },
+  { 
+    id:2,
+    name:"Samgyepsal",
+    image:"https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg"
+  },{ 
+    id:3,
+    name:"ramen",
+    image:"https://health.chosun.com/site/data/img_dir/2020/07/21/2020072103114_0.jpg"
+  }
+
+]
+
+
+
+
 
 function App() {
   return (
     <div>
-      <h1>hello!</h1>
-      <Food fav="kimchi"/>
-      <Food fav="ramen"/>
-      <Food fav="samgiopsal"/>
-      <Food fav="chukumi" something={true} papa={["hello",1,2,true]}/>
+      {foodILike.map(dish =>(
+        <Food key={dish.id} name={dish.name} picture={dish.image}/>
+      ))}
     </div>
   );
 }
